9864dd879d5374aa59bc57f5457b6f573d2a68de
Logan <490211581@qq.com>
2025-03-05 18:12:12 +0800

    自动导入 vue、uni-app包

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    AutoImport({
+      // 自动导入的包
+      imports: ["vue", "uni-app"],
+    }),
+  ],
 });
