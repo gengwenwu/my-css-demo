16c9e46ae70e3b301e84b57c58561cd74340e019
Logan <490211581@qq.com>
2025-03-19 16:53:22 +0800

    添加 banner 案例

--- src/pages/flex/travel/index.vue
+++ src/pages/flex/travel/index.vue
@@ -1,8 +1,14 @@
 <template>
   <view class="content">
+    <!-- 1.头部搜索 -->
     <view class="search-index">
       <view class="search">搜索:目的地/酒店/景点/航班号 </view>
       <view class="user">我 的</view>
+    </view>
+
+    <!-- 2.banner -->
+    <view class="focus">
+      <image class="focus-img" src="./upload/focus.jpg" mode="widthFix" />
     </view>
   </view>
 </template>
