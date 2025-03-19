67e43232065f28c71c258cefae00b9995f91a1f4
Logan <490211581@qq.com>
2025-03-19 19:05:03 +0800

    添加局部导航栏案例

--- src/pages/flex/travel/index.vue
+++ src/pages/flex/travel/index.vue
@@ -10,6 +10,32 @@
     <view class="focus">
       <image class="focus-img" src="./upload/focus.jpg" mode="widthFix" />
     </view>
+
+    <!-- 3. 局部导航栏 -->
+    <view class="local-nav">
+      <view class="local-nav-child">
+        <view class="local-nav-child-icon1"></view>
+        <view class="text">景点玩乐</view>
+      </view>
+      <view class="local-nav-child">
+        <view class="local-nav-child-icon2"></view>
+        <view class="text">周边游</view>
+      </view>
+      <view class="local-nav-child">
+        <view class="local-nav-child-icon3"></view>
+        <view class="text">美食杯</view>
+      </view>
+      <view class="local-nav-child">
+        <view class="local-nav-child-icon4"></view>
+        <view class="text">一日游</view>
+      </view>
+      <view class="local-nav-child">
+        <view class="local-nav-child-icon5"></view>
+        <view class="text">当地攻略</view>
+      </view>
+    </view>
+
+    <!-- 4，  -->
   </view>
 </template>
 
